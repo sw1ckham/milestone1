8f3b47d11d4e904fb864aa17b5d529ec90dbf0c3
Sophie <sphwick@gmail.com>
2020-05-05 13:27:23 +0100

    Added email js file

---
+++ assets/js/contact.js
@@ -0,0 +1,25 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "southwicks", {
+        "from_firstname": contactForm.firstName.value,
+        "from_secondname": contactForm.secondName.value,
+        "from_email": contactForm.email.value,
+        "from_telephone": contactForm.telephone.value,
+        "from_address": contactForm.address.value,
+        "from_postcode": contactForm.postCode.value,
+        "message": contactForm.message.value
+    })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            },
+            function(error) {
+                console.log("FAILED", error);
+            }
+
+        );
+    return false;
+}
+
+$(".dropdown-menu")click(function(){
+    $(".navbar-dropdown-heading").css("background-color", "green");
+})
