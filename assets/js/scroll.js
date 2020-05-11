fb173469aa9f2c2ea795d02dc57943382f5bdf5a
Sophie Wickham <sphwick@gmail.com>
2020-05-11 13:40:41 +0000

    Added content asked to by client

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,18 +1,3 @@
-// $(document).ready(function(){
-
-// var controller = new ScrollMagic.Controller();
-
-// new ScrollMagic.Scene({
-// 							triggerElement: "#trigger1",
-// 							triggerHook: 0.9, // show, when scrolled 10% into view
-// 							duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-// 							offset: 50 // move trigger to center of element
-// 						})
-// 						.setClassToggle("#reveal1", "visible") // add class to reveal
-// 						.addTo(controller);
-
-
-// });
 
 
 $(document).ready(function(){
