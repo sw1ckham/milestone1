57cd46b51cce67c98303c151006a0b40265e5a3d
Sophie Wickham <sphwick@gmail.com>
2020-05-13 11:37:10 +0000

    Taken away unnessecary forms and unminified contact js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,1 +1,17 @@
-$(document).ready(function(){window.onload=function(){document.getElementById("contact-form").addEventListener("submit",function(e){e.preventDefault(),emailjs.sendForm("contact_service","southwicks","#contact-form").then(function(e){200==e.status&&"OK"==e.text?alert("Your message has been sent successfully!"):alert("Sorry there was a problem, please refresh your browser and try again."),document.getElementById("contact-form").reset(),window.location.replace("index.html")})})}});
+$(document).ready(function(){
+            window.onload = function() {
+            document.getElementById('contact-form').addEventListener('submit', function(event){
+                event.preventDefault();
+                emailjs.sendForm('contact_service', 'southwicks', '#contact-form')
+                .then(function(response) {
+                    if (response.status == 200 && response.text == 'OK')
+                        alert('Your message has been sent successfully!');
+                    else 
+                        alert('Sorry there was a problem, please refresh your browser and try again.');
+                        document.getElementById('contact-form').reset();
+                        window.location.replace("index.html");
+                });
+            });
+        };
+});
+
