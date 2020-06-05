a239c4ee2f1238edb4be31bf62eeb9fac3b34ec6
Sophie Wickham <sphwick@gmail.com>
2020-06-05 11:33:17 +0000

    Added new email API for email transfer

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
             window.onload = function() {
             document.getElementById('contact-form').addEventListener('submit', function(event){
                 event.preventDefault();
-                emailjs.sendForm('contact_service', 'southwicks', '#contact-form')
+                emailjs.sendForm('contact_service', 'website_template', '#contact-form')
                 .then(function(response) {
                     if (response.status == 200 && response.text == 'OK')
                         alert('Your message has been sent successfully!');
